f3841b5a45e242754fd8840e4dc63926a16b43a0
fawzi <fawzi@belorder.com>
2023-01-29 21:07:10 +0100

    [feat] fetch loader

--- lib/components/PostsList.tsx
+++ lib/components/PostsList.tsx
@@ -1,22 +1,30 @@
 import { FC } from 'react'
 import InfiniteScroll from 'react-infinite-scroller'
-import { Container, Row } from 'reactstrap'
+import { Row, Spinner } from 'reactstrap'
 
 import { getPostStore } from '../stores/post.store'
 import { PostCard } from './PostCard'
+
+const InfiniteScrollLoader: FC = () => (
+  <Row className="mt-4 p-2 justify-content-center">
+    <Spinner />
+  </Row>
+)
 
 export const PostsList: FC = () => {
   const { posts, hasMore, getNextPosts } = getPostStore()
 
   return (
-    <Container>
-      <InfiniteScroll hasMore={hasMore} loadMore={getNextPosts} initialLoad={false} threshold={700}>
-        <Row>
-          {posts.map((post) => (
-            <PostCard key={post.id} post={post} />
-          ))}
-        </Row>
-      </InfiniteScroll>
-    </Container>
+    <InfiniteScroll
+      hasMore={hasMore}
+      loadMore={getNextPosts}
+      initialLoad={false}
+      loader={<InfiniteScrollLoader />}>
+      <Row>
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </Row>
+    </InfiniteScroll>
   )
 }
