13835f9704454563e41a3feef10982e6e8415cfc
fawzi <fawzi@belorder.com>
2023-01-29 23:41:05 +0100

    [refactor] Fix infinite scroll list loader

--- lib/components/PostsList.tsx
+++ lib/components/PostsList.tsx
@@ -6,20 +6,22 @@ import { getPostStore } from '../stores/post.store'
 import { PostCard } from './PostCard'
 
 const InfiniteScrollLoader: FC = () => (
-  <Row className="mt-4 p-2 justify-content-center">
+  <Row key={0} className="mt-4 p-2 justify-content-center">
     <Spinner />
   </Row>
 )
 
 export const PostsList: FC = () => {
-  const { posts, hasMore, getNextPosts } = getPostStore()
+  const { posts, hasMore, isLoading, getNextPosts } = getPostStore()
 
   return (
-    <InfiniteScroll hasMore={hasMore} loadMore={getNextPosts} loader={<InfiniteScrollLoader />}>
+    <InfiniteScroll hasMore={hasMore} loadMore={getNextPosts}>
       <Row>
         {posts.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
+
+        {isLoading && <InfiniteScrollLoader />}
       </Row>
     </InfiniteScroll>
   )
